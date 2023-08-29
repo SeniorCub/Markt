35775422cf55bfde09a2c1dfd5eb785986565f53
SeniorCub <farinderif@gmail.com>
2023-08-29 18:35:34 +0100

    I used an API to fetch different products for the deals

---
+++ script.js
@@ -0,0 +1,41 @@
+// const imag = document.querySelector('.card-img-top')
+// const descrip = document.querySelector('.card-text')
+// const cartgo = document.querySelector('.cartgo')
+// const original = document.querySelector('.original')
+
+// fetch('https://fakestoreapi.com/products?limit=5')
+// .then(res=>res.json())
+// .then(data2 => data2.forEach(element => {
+//      imag.src = element.image
+//      descrip.innerText = element.title
+//      cartgo.innerText = element.category
+//      original.innerText = element.price
+// }))
+
+
+fetch('https://fakestoreapi.com/products?limit=12')
+.then (data => data.json())
+.then(data2 => data2.forEach(element => {
+document.querySelector('.cards').innerHTML += `
+     <div class="card" style="width: 20rem; height: 30rem;">
+          <div class="bon card-title d-flex">
+               <a href="#" class="btn btn-pri percent">-59%</a>
+               <a href="#" class="btn btn-pri love"><i class="fa-regular fa-heart" style="color: #e94c2a;"></i></a>
+          </div>
+
+          <img src= ${element.image}>
+
+          <div class="card-title  d-flex">
+               <a href="#" class="btn btn-sm btn-pri cartgo">${element.category}</a>
+               <h5 class="original">$${element.price}</h5>
+          </div>
+          
+          <p class="card-text">${element.title}</p>
+
+          <div class="bon card-title d-flex">
+               <a href="#" class="btn btn-pri">Add to Cart</a>
+               <a href="#" class="btn btn-primary">Buy Now</a>
+          </div>
+     </div>
+`
+}))
