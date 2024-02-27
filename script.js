5de0a5e8cd4275d2b21f9dcbdfc7a12d6988d34c
seniorcub <farinderif@gmail.com>
2024-02-27 03:18:26 +0100

    MAde little review on google meet

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.querySelector('.cards').innerHTML += `
 
           <button class="CartBtn">
                <span class="IconContainer"> 
-               <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
+               <i  class="fa-solid fa-cart-shopping" class="cart" style="color: #ffffff;"></i>
                </span>
                <span class="text">Add to Cart</span>
           </button>
@@ -47,7 +47,8 @@ let heroP = document.querySelector('#hero .p');
 let secBox = document.querySelector('.section');
 let firstSecHead = document.querySelector('#first-section-heading');
 let dealss = document.querySelector('.dealss');
-let caa = document.querySelector('.card');
+
+let cardd = document.querySelectorAll('.card');
 
 document.querySelector('.theme').addEventListener('click', change) 
 function change() {
@@ -58,6 +59,10 @@ function change() {
           secBox.style.backgroundColor = "#101010";
           firstSecHead.style.color = "#f1f1f1";
           dealss.style.backgroundColor = "#101010";
+          cardd.forEach(element => {
+               element.style.backgroundColor = "#101010";
+               element.style.color = "#f1f1f1";
+          });
      } else {
           console.log("not clicked");
           hero.style.backgroundColor = "#f1f1f1";
@@ -65,5 +70,9 @@ function change() {
           secBox.style.backgroundColor = "#f1f1f1";
           firstSecHead.style.color = "#000";
           dealss.style.backgroundColor = "#f1f1f1";
+          cardd.forEach(element => {
+               element.style.backgroundColor = "#f1f1f1";
+               element.style.color = "#000";
+          });
      }
 }
