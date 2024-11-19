8325a3e3d5815e022c524828835ece73bc0c24d7
seniorcub <farinderif@gmail.com>
2024-11-19 16:00:32 +0100

    Enhance Tailwind configuration and accessibility settings; add new SVG icons and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
      content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+     extend: {
+          container: {
+               center: true,
+          },
+          colors: {
+               'primary': '#DB4444',
+               'dark': '#121212',
+               'gray': '#f6f5f4',
+               'gray2': "#F0EEED",                                                   
+          }
+     },
   },
   plugins: [],
 }
